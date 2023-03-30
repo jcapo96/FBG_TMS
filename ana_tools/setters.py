4df12833c6d70c24e523ccaca1925c141649ef02
Jordi Capo Torres <jcapotor@lxplus796.cern.ch>
2023-03-30 16:00:37 +0200

    removed files

--- ana_tools/setters.py
+++ ana_tools/setters.py
@@ -2,5 +2,21 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def set_time_window(df):
-    return None
+def add_polarisation_mask(data):
+    first_row = data["Wav1-1"][0]
+    second_row = data["Wav1-1"][1]
+
+    polarisation_mask = []
+    if first_row > second_row:
+        first_pol = "p"
+        second_pol = "s"
+    else:
+        first_pol = "s"
+        second_pol = "p"
+    for i in range(len(data)):
+        if i % 2 == False:
+            polarisation_mask.append(first_pol)
+        else:
+            polarisation_mask.append(second_pol)
+    data["PolMask"] = polarisation_mask
+    return data
